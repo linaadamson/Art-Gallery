19baf9ad86097280ebd39b9d00e011b58ad6bdba
linaadamson <Adamsonlina@gmail.com>
2023-11-04 12:12:43 +0000

    Feat: add camera movements wip

---
+++ app.js
@@ -0,0 +1,135 @@
+import {imageDetails} from './imageDetails.js'
+
+function objectToPos(object) {
+  return object.x + " " + object.y + " " + object.z;
+}
+
+function objectToRot(object) {
+  return object.x + " " + object.y + " " + object.z;
+}
+function cameraToRot(object) {
+  return object.x + " " + object.y + " " + object.z;
+}
+
+function cameraToPos(object) {
+  if (object.x < 0) {
+    return object.x + 1.2 + " " + 0 + " " + object.z;
+  }
+  return object.x - 1.2 + " " + 0 + " " + object.z;
+}
+
+function clickHandler() {
+  let rig = document.querySelector("#rig");
+  let camera = document.querySelector("#camera");
+  camera.setAttribute("look-controls", "enabled", false);
+  camera.setAttribute("wasd-controls", "enabled", false);
+  camera.setAttribute("position", "0 1.6 0");
+  camera.setAttribute("rotation", "0 0 0");
+
+  let targetPos = cameraToPos(this.getAttribute("position"));
+  let targetRot = cameraToRot(this.getAttribute("rotation"));
+  let rigPos = objectToPos(rig.getAttribute("position"));
+  let rigRot = objectToRot(rig.getAttribute("rotation"));
+
+  rig.setAttribute(
+    "animation",
+    `property:position; from: ${rigPos}; to: ${targetPos}; dur: 1000`
+  );
+
+  rig.setAttribute(
+    "animation__2",
+    `property:rotation; from: ${rigRot}; to: ${targetRot}; dur: 1000`
+  );
+
+  camera.setAttribute(
+    "animation__3",
+    `property:rotation; to: 0 0 0; dur: 1000`
+  );
+  camera.setAttribute(
+    "animation__4",
+    `property:position; to: 0 1.6 0; dur: 700`
+  );
+
+  openPopup(this);
+
+  console.log("click", objectToRot(camera.getAttribute("rotation")));
+}
+
+function popupToPos(object) {
+  if (object.x < 0) {
+    return object.x + 0.1 + " " + object.y + " " + (object.z - 1.34);
+  }
+
+  return object.x - 0.1 + " " + object.y + " " + (object.z - 1.34);
+}
+
+
+function openPopup(target) {
+  let title = document.querySelector("#title");
+  let description = document.querySelector("#description");
+  let popup = document.querySelector("#popup");
+
+  const selectedImg = imageDetails.find((item) => item.id === target.id);
+
+  let targetPos = popupToPos(target.getAttribute("position"));
+  let targetRot = cameraToRot(target.getAttribute("rotation"));
+
+  title.setAttribute("text", "value", selectedImg?.title);
+  description.setAttribute("text", "value", selectedImg?.description);
+  popup.setAttribute("animation", `property:visible; from: false; to: true;`);
+  popup.setAttribute("rotation", targetRot);
+  popup.setAttribute("position", targetPos);
+}
+
+function closePopupHandler() {
+  let rig = document.querySelector("#rig");
+  let camera = document.querySelector("#camera");
+  let popup = document.querySelector("#popup");
+  let rigPos = objectToPos(rig.getAttribute("position"));
+  let rigRot = objectToRot(rig.getAttribute("rotation"));
+  camera.setAttribute("look-controls", "enabled", true);
+  camera.setAttribute("wasd-controls", "enabled", true);
+  camera.setAttribute(
+    "animation__3",
+    `property:rotation; to: 0 0 0; dur: 1000`
+  );
+  camera.setAttribute(
+    "animation__4",
+    `property:position; to: 0 1.6 0; dur: 700`
+  );
+
+  popup.setAttribute("animation", `property:visible; from: true; to: false;`);
+  popup.setAttribute("position", "0 0 0");
+
+  rig.setAttribute(
+    "animation",
+    `property:position; from: ${rigPos}; to: 0 0 3; dur: 1000`
+  );
+
+  rig.setAttribute(
+    "animation__2",
+    `property:rotation; from: ${rigRot}; to:0 0 0 ; dur: 1000`
+  );
+
+  console.log("close", objectToRot(camera.getAttribute("rotation")));
+}
+
+AFRAME.registerComponent("move-camera", {
+  init: function () {
+    this.el.addEventListener("click", clickHandler);
+  },
+
+  remove: function () {
+    this.el.removeEventListener("click", clickHandler);
+  },
+});
+
+AFRAME.registerComponent("close-popup", {
+  init: function () {
+    this.el.addEventListener("click", closePopupHandler);
+  },
+
+  remove: function () {
+    this.el.removeEventListener("click", closePopupHandler);
+  },
+});
---
+++ imageDetails.js
@@ -0,0 +1,39 @@
+export const imageDetails = [
+    {
+      id: "image1",
+      title: "Heavenly",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consequatur nam soluta tempora veniam labore consequuntur laboriosam iste cum molestias, corrupti omnis odit dolores ipsa ipsum provident maxime dignissimos accusantium quam fuga, fugiat, voluptatibus numquam? Illo iste ipsam nulla quae eveniet eligendi? Temporibus distinctio minima odit praesentium sunt, inventore soluta.",
+    },
+    {
+      id: "image2",
+      title: "Light",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consequatur nam soluta tempora veniam labore consequuntur laboriosam iste cum molestias, corrupti omnis odit dolores ipsa ipsum provident maxime dignissimos accusantium quam fuga, fugiat, voluptatibus numquam? Illo iste ipsam nulla quae eveniet eligendi? Temporibus distinctio minima odit praesentium sunt, inventore soluta.",
+    },
+    {
+      id: "image3",
+      title: "Sirens",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consequatur nam soluta tempora veniam labore consequuntur laboriosam iste cum molestias, corrupti omnis odit dolores ipsa ipsum provident maxime dignissimos accusantium quam fuga, fugiat, voluptatibus numquam? Illo iste ipsam nulla quae eveniet eligendi? Temporibus distinctio minima odit praesentium sunt, inventore soluta.",
+    },
+    {
+      id: "image4",
+      title: "Cosmic",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consequatur nam soluta tempora veniam labore consequuntur laboriosam iste cum molestias, corrupti omnis odit dolores ipsa ipsum provident maxime dignissimos accusantium quam fuga, fugiat, voluptatibus numquam? Illo iste ipsam nulla quae eveniet eligendi? Temporibus distinctio minima odit praesentium sunt, inventore soluta.",
+    },
+    {
+      id: "image5",
+      title: "Walking",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consequatur nam soluta tempora veniam labore consequuntur laboriosam iste cum molestias, corrupti omnis odit dolores ipsa ipsum provident maxime dignissimos accusantium quam fuga, fugiat, voluptatibus numquam? Illo iste ipsam nulla quae eveniet eligendi? Temporibus distinctio minima odit praesentium sunt, inventore soluta.",
+    },
+    {
+      id: "image6",
+      title: "Create",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni consequatur nam soluta tempora veniam labore consequuntur laboriosam iste cum molestias, corrupti omnis odit dolores ipsa ipsum provident maxime dignissimos accusantium quam fuga, fugiat, voluptatibus numquam? Illo iste ipsam nulla quae eveniet eligendi? Temporibus distinctio minima odit praesentium sunt, inventore soluta.",
+    },
+  ];
+  
